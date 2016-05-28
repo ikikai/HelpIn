c6aa952867d25d0680e11f4dfde2231d9bbc384b
Gerasim-Stan <gerasim.stanchev@gmail.com>
2016-05-28 17:27:53 +0300

    Basic chat feature

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,2 @@
 class Client < ActiveRecord::Base
-
-  def name
-    'baba'
-  end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_HelpIn_session'
+SockChat::Application.config.session_store :cookie_store, key: '_sock-chat_session'
